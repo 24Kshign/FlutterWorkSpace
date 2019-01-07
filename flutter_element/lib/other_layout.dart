bd7e0a1abdbb0b894da55b3c54c1966c22ffa6e4
CYG <17764576259@163.com>
2019-01-07 15:36:05 +0800

    last commit

---
+++ flutter_element/lib/other_layout.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class OtherLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
+
+class OtherLayoutElement extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _OtherLayoutElement();
+  }
+}
+
+class _OtherLayoutElement extends State<OtherLayoutElement> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
